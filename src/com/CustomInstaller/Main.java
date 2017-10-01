e4dfd88c48fff400c6535b689e63555c611417f3
JudgeGlass <hdwilcox02@gmail.com>
2017-10-01 19:58:34 -0400

    Fixed text formatting issues

--- src/com/CustomInstaller/Main.java
+++ src/com/CustomInstaller/Main.java
@@ -4,7 +4,17 @@ import com.CustomInstaller.windows.InitWindow;
 
 public class Main {
 	public static void main(String args[]) {
-		
+		System.out.println(System.getProperty("os.name"));
+		try {
+			if(!System.getProperty("os.name").equals("Linux")) {
+				UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			}
+			UIManager.put("Button.font", new Font(Font.SANS_SERIF, 0, 11));
+			UIManager.put("Label.font", new Font(Font.SANS_SERIF, 0, 11));
+			UIManager.put("RadioButton.font", new Font(Font.SANS_SERIF, 0, 11));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 		InitWindow window = new InitWindow();
 		window.setVisible(true);
 		
